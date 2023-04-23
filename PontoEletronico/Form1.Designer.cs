17487d22e59e6f78cfefbbb4e5e02b4d94574088
Cleison Nunes <cleison.nunes20@gmail.com>
2023-04-23 18:11:26 -0300

    Adicionar arquivos de projeto.

---
+++ PontoEletronico/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PontoEletronico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(617, 187);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Marcar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Almoço",
+            "Banheiro",
+            "Pausa"});
+            this.comboBox1.Location = new System.Drawing.Point(37, 242);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tipo de marcação";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Entrada";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(272, 300);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 335);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PontoEletronico/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PontoEletronico
+{
+    public partial class Form1 : Form
+    {
+        int btnClicks = 0;
+        bool almocei = false;
+        bool pausei = false;
+        bool banheiro = false;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                tipoPausa();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Entrada")
+            {
+                Entrar();
+            }
+            else
+            {
+                Sair();
+            }
+        }
+
+
+        private void Entrar()
+        {
+            button2.Text = "Saída";
+            DateTime dia = DateTime.Now;
+            string horas = string.Format("{0:T}", dia);
+            string hoje = string.Format("{0:d}", dia);
+
+            int horaDeEntrar = 18;
+            if(horaDeEntrar == DateTime.Now.Hour)
+            {
+                textBox1.Text = $"Entrada hoje às {horas}, Dia:{hoje} ";
+                comboBox1.Enabled = true;
+            }
+            else
+            {
+                textBox1.Text = $"Horário de entrada até as 10:00";
+            }
+
+        }
+
+        private void tipoPausa()
+        {
+          
+            DateTime dia = DateTime.Now;
+            string horas = string.Format("{0:T}", dia);
+            string volta = string.Format("{0:T}", dia.AddHours(1));
+            string voltaPausa = string.Format("{0:T}", dia.AddMinutes(15));
+            string hoje = string.Format("{0:d}", dia);
+           
+
+            //PAUSA ALMOÇO
+            if (comboBox1.SelectedItem == "Almoço" && almocei == false)
+            {
+                almocei = true;
+                button1.Text = "Volta almoço";
+                textBox1.AppendText(System.Environment.NewLine + $"Saiu para almoçar  às {horas}, volte até às {volta} ");
+            }else if(comboBox1.SelectedItem == "Almoço" && almocei == true)
+            {
+                button1.Text = "Marcar";
+                textBox1.AppendText(System.Environment.NewLine + $"Voltou do almoço  às {horas} ");
+            }
+
+            //PAUSA BANHEIRO
+            if (comboBox1.SelectedItem == "Banheiro" && banheiro == false)
+            {
+                banheiro = true;
+                button1.Text = "Volta banheiro";
+                textBox1.AppendText(System.Environment.NewLine + $"Pausa para ir ao banheiro");
+            }
+            else if (comboBox1.SelectedItem == "Banheiro" && banheiro == true)
+            {
+                button1.Text = "Marcar";
+                textBox1.AppendText(System.Environment.NewLine + $"Voltou da pausa banheiro");
+            }
+
+            //PAUSA DE 15 MINUTOS
+            if (comboBox1.SelectedItem == "Pausa" && pausei == false)
+            {
+                pausei = true;
+                button1.Text = "Pausa 15 min";
+                textBox1.AppendText(System.Environment.NewLine + $"Pausa de 15 minutos , retorne até às {voltaPausa} ");
+            }
+            else if (comboBox1.SelectedItem == "Pausa" && pausei == true)
+            {
+                button1.Text = "Marcar";
+                textBox1.AppendText(System.Environment.NewLine + $"Voltou da pausa de 15 minutos às {volta} ");
+            }
+        }
+
+        private void Sair()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            DateTime dia = DateTime.Now;
+            string horas = string.Format("{0:T}", dia);
+            string hoje = string.Format("{0:d}", dia);
+
+          
+            textBox1.AppendText (System.Environment.NewLine + $"Saída  às {horas}, Dia:{hoje} ");
+ 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
